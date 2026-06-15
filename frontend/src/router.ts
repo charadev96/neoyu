6635cad33006bedeb31c4134d02af7a8f78e1586
charadev96 <charadev96@gmail.com>
2026-06-15 21:09:34 +0300

    feat(home): Add placeholder view

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,8 +1,12 @@
 import { createWebHistory, createRouter } from "vue-router"
 
+import ViewHome from "@/views/ViewHome.vue"
 import ViewConnections from "@/views/ViewConnections.vue"
 
-const routes = [{ path: "/connections", component: ViewConnections }]
+const routes = [
+  { path: "/", component: ViewHome },
+  { path: "/connections", component: ViewConnections },
+]
 
 export const router = createRouter({
   history: createWebHistory(),
